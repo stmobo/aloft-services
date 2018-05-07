15dd7a0a61104adb4e34b2235326ca8100a95d64
Sebastian Mobo <stmobo@gmail.com>
2018-05-06 20:39:08 -0500

    Add file logging

--- aloft_services/__init__.py
+++ aloft_services/__init__.py
@@ -1,4 +1,32 @@
 from flask import Flask
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {
+        'wsgi': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://flask.logging.wsgi_errors_stream',
+            'formatter': 'default'
+        },
+        'file': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'default',
+            'filename': 'log/app.log',
+            'maxBytes': 64 * 1024 * 1024,
+            'backupCount': 3,
+        }
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi', 'file']
+    }
+})
+
+
 app = Flask('aloft_services')
 app.config['MAX_CONTENT_LENGTH'] = 64 * 1024 * 1024
 
