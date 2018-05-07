6417c736a30908980dce52d7d4e2179bcb7de950
Sebastian Mobo <stmobo@gmail.com>
2018-05-06 19:51:49 -0500

    test another dumb change

--- aloft_services/__init__.py
+++ aloft_services/__init__.py
@@ -3,6 +3,6 @@ app = Flask('aloft_services')
 
 @app.route('/')
 def index():
-    return "There may possibly be stuff here, soon."
+    return "There may possibly be stuff here, at some point."
 
 import aloft_services.pdf_rotate
