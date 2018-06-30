2bd9a23d1e2384345d936d332664fff40b239ae1
Sebastian Mobo <stmobo@gmail.com>
2018-06-30 12:37:13 -0500

    c2x_iface: fix zipfile writing

--- aloft_services/c2x_iface.py
+++ aloft_services/c2x_iface.py
@@ -38,7 +38,7 @@ def receive_files():
 
     out_io = BytesIO()
 
-    with zf.ZipFile(out_io) as zip_out:
+    with zf.ZipFile(out_io, 'w') as zip_out:
         with zip_out.open('opponent.xml', 'w') as opponent_out:
             opponent_out.write("<?xml version='1.0' encoding='UTF-8'?>\n")
             opponent_out.write('<!-- '+c2x.generate_comment()+' -->\n\n')
